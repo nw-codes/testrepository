8511beb0139d453ea04d17bf67f41b3e053158b8
nw-codes <Nick.werner94@web.de>
2023-04-18 21:48:20 +0200

    leicht abgeändert

--- simplecppprogram.cc
+++ simplecppprogram.cc
@@ -5,7 +5,7 @@ int main()
 
 std::cout << "Dies ist mein erstes C++ Programm auf Github." << std::endl;
 
-// Dies ist eine Änderung zu Demonstrationszwecken
+// Dies ist eine Änderung zu Demonstrationszwecken, Test 2
 
     return 0;
 }
